c6fcb24fcf6e230e5ac6cf5b0bfa099a5d62f5bc
Joell Alex <joellalex922@gmail.com>
2026-04-19 22:11:58 +0530

    added projects section will update later

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,7 +3,12 @@ export default function Projects() {
     <section id="projects" className="mt-10">
       <h2 className="text-3xl font-bold">Projects</h2>
         <p className="text-gray-300 mt-2">
-          Here are some of my projects.
+          Will update this section soon, in the mean time please feel free to look at all my available repos.
+          <p>
+          <a href="https://github.com/Joell-Alex1?tab=repositories" className="text-blue-500 text-2xl font-bold hover:underline">
+            GitHub
+          </a>
+          </p>
         </p>
     </section>
   );
